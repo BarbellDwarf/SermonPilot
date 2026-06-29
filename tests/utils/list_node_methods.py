33fc9875f45cf02926d20adb5895c245e961b174
SpirusNox <>
2026-06-29 07:34:57 -0500

    Remove test files from repo, add tests/ to .gitignore

--- tests/utils/list_node_methods.py
+++
@@ -1,7 +0,0 @@
-import inspect
-
-from sermonaudio.node import requests as node_requests
-
-print("Node methods:")
-for name, func in inspect.getmembers(node_requests.Node, inspect.isfunction):
-    print(name)
